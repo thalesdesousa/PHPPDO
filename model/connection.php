2dd4ddc49de8444a50fa0062716eb595b2cbb5ee
thalesdesousa <thalessistinfo@gmail.com>
2015-07-06 15:12:14 -0300

    Mudança das funções para connect

--- model/connection.php
+++ model/connection.php
@@ -9,7 +9,35 @@
    
         return $pdo;
     }
-	
+function inserir($nome, $senha, $email,$pdo){
+    $inserir = $pdo->prepare("INSERT INTO `sistemahx`."
+                            . "`usuarios` (`ID`,`NAME`,`PASS`,`EMAIL`)"
+                            . " VALUES (NULL, :name, :pass, :email);");
+    
+    $inserir->bindValue(":name", $nome);
+    $inserir->bindValue(":pass", $senha);
+    $inserir->bindValue(":email", $email);
+
+        $validar=$pdo->prepare("SELECT * FROM `sistemahx`.`usuarios`"
+                . " WHERE email=?");
+    $validar->execute(array($email));
+
+    if($validar->rowCount()==0){            
+        $inserir->execute();
+
+    }else{
+        return 'Email já Cadastrado!';}
+}
+function listar($pdo){
+    $buscarUsuario = $pdo->prepare("SELECT * FROM `sistemahx`.`usuarios`");
+    $buscarUsuario->execute();
+    
+    
+    while($linha = $buscarUsuario->fetch(PDO::FETCH_ASSOC)){
+        echo "Nome: ".$linha['NAME']."<br />";
+        echo "Email: ".$linha['EMAIL']."<P />";
+    }
+    }
 	/*---------------------------modo errado de conexao--------------------------
 	$host = "localhost";
 	$user = "root";
